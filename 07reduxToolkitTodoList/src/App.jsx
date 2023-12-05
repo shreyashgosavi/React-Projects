3be94eec90282cae65fb76a3d2b29667cba7496e
shreyashgosavi <shreyashgosavi33@gmail.com>
2023-12-05 21:14:01 +0530

    redux toolkit project

---
+++ 07reduxToolkitTodoList/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/addTodo'
+import Todos from './components/todos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>TodoList with Redux Toolkit </h1>
+     <AddTodo />
+     <Todos />
+    </>
+  )
+}
+
+export default App
---
+++ 07reduxToolkitTodoList/src/features/todo/todoSlice.js
@@ -0,0 +1,36 @@
+import { createSlice , nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos : [{id: 1 ,text: "hello world"}]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers:{
+        addTodo: (state , action) => {
+            const todo ={
+                id: nanoid(),
+                text:action.payload
+            }
+            state.todos.push(todo)
+
+        },
+        removeTodo: (state , action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        
+        },
+        updateTodo: (state, action) => {
+            const todo ={ 
+                id:nanoid(),
+                text:action.payload
+            }
+            state.todos.puch(todo)
+
+        }
+    }
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
